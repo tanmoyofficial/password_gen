73c977efb2eb21e543c1b05e565f6f0287ddb2ec
Tanmoy <tanmoyofficial7@gmail.com>
2024-01-03 10:33:15 +0530

    issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 py-3 text-orange-500 bg-gray-200 ">
-        <h1 className="text-center text-gray-800 my-3 text-2xl ">Password Generotor</h1>
+        <h1 className="text-center text-gray-800 my-3 text-2xl ">Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
             type="text"
